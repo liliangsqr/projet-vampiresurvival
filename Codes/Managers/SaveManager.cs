07130af5a51d12614d52136def2b335454ce8028
Pingrammer <108672186+Pingrammer@users.noreply.github.com>
2024-10-04 01:10:49 +0200

    ++

---
+++ Codes/Managers/SaveManager.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+using System.IO;
+
+public partial class SaveManager : Node2D
+{
+	public void save(string filename){
+		
+	}
+	
+	public void load(string filename){
+		
+	}
+}
