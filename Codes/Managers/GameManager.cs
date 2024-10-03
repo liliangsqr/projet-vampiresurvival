f78bb945a04c4bf9441eefd4df60059a7dae04e0
Pingrammer <108672186+Pingrammer@users.noreply.github.com>
2024-10-04 00:49:40 +0200

    levelManager

---
+++ Codes/Managers/GameManager.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class GameManager : SceneTree
+{
+	static GameManager game;
+	private LevelManager level;
+	private SaveManager save;
+	
+	private GameManager(){}
+
+	public override void _Initialize()
+	{
+		GD.Print("Initialized:");
+		getInstance();
+		level = new LevelManager();
+		save = new SaveManager();
+		level.load("res://Codes/Menus/MenuPrincipal.tscn");
+	}
+	public static GameManager getInstance(){
+	
+		if (game == null){
+			game = new GameManager();
+		}
+		return game;	
+	}
+	
+	public LevelManager getLevelManager(){
+		return level;
+	}
+	
+	public LevelManager getSaveManager(){
+		return save;
+	}
+		
+}
