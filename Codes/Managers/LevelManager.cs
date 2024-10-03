f162afe35f9aee4a72b6796a9c487ba0f8732d58
Pingrammer <108672186+Pingrammer@users.noreply.github.com>
2024-10-03 23:03:50 +0200

    Level Manager v1

---
+++ Codes/Managers/LevelManager.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class LevelManager : Node2D
+{
+	public void load(string str_scene){
+		try 
+		{
+		  change_scene_to_file(str_scene);
+		}
+		catch (Exception e)
+		{
+		  GD.Print("Level can't be found");
+		}
+		
+	}
+}
